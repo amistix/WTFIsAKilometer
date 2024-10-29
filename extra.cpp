494d78c2aea77a206df79a2ef41a2e20c4e25fdd
Amistix <nazartysh2016@gmail.com>
2024-10-29 14:11:22 +0200

    New

---
+++ extra.cpp
@@ -0,0 +1,28 @@
+namespace ext
+{
+    class Point
+    {
+        public:
+            int x;
+            int y;
+
+            Point(int x = 0, int y = 0)
+            {
+                this->x = x;
+                this->y = y;
+            }
+
+            Point operator - (Point &other)
+            {
+                return Point(this->x - other.x, this->y - other.y);
+            }
+            Point operator + (Point &other)
+            {
+                return Point(this->x + other.x, this->y + other.y);
+            }
+            int operator * (Point &other)
+            {
+                return this->x * other.x + this->y * other.y;
+            } 
+    };
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include "stdafx.h"
+#include "extra.cpp"
 using namespace std;
+using namespace ext;
+
 
 int main(int argc, char** argv)
 {
-    cout << "Hallo world" << endl;
+    Point a(2, 4);
+    Point b(3, 10);
+    cout << a * b << endl;
+    return 0;
 }
